9541883202ed43a58ad2e617ddae6b62a168a6f9
y.sergukova <y.sergukova@corp.mail.ru>
2017-08-23 15:30:19 +0300

    extract long/lat

--- info_extractor/parser.py
+++ info_extractor/parser.py
@@ -9,23 +9,28 @@ soup = BeautifulSoup(open('example.html', 'r'), 'html.parser')
 address=""
 district=""
 houses=[]
-ul=soup.find('ul', class_="information")
+coords={}
+ul=soup.find('ul', {"class" : "information"})
 el_li=ul.li
 while not el_li is None:
     if hasattr(el_li, 'b'):
-        if el_li.b.string == u"Адрес:":
+        if el_li.b.string == "Адрес:":
             raw=el_li.span.string
             k=raw.rfind(', ')
             address=raw[k+2:]
-        if el_li.b.string == u"Район города:":
+        elif el_li.b.string == "Район города:":
             district=el_li.span.string
-        if el_li.b.string == u"Номера домов:":
+        elif el_li.b.string == "Номера домов:":
             raw=el_li.span.string
             houses=raw.replace(' ', '').replace('.', '').split(',')
     el_li=el_li.find_next_sibling('li')
 
-print address,"\n",district,"\n",houses
+meta=soup.find('meta', {"name" : "og:image"})
+url=meta['content']
+for el in url.split('?')[1].split('&'):
+    if 'll=' in el:
+        parts=el.split('=')[1].split(',')
+        coords['n'] = float(parts[1])
+        coords['e'] = float(parts[0])
 
-for div in soup.find_all('div', class_="content-block"):
-    if hasattr(div, 'h2') and hasattr(div, 'p'):
-        print div.p
+print(address, '\n', district, '\n', houses, '\n', coords)
