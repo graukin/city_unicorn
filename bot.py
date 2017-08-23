d6bf62a62093b74dd05df04514f1edaa8e0ac281
y.sergukova <y.sergukova@corp.mail.ru>
2017-08-23 18:25:54 +0300

    add grep

--- bot.py
+++ bot.py
@@ -3,12 +3,15 @@
 import logging
 import os
 import sys
+import sqlite3
 
 from telegram.parsemode import ParseMode
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARN)
 logger = logging.getLogger(__name__)
+
+conn = sqlite3.connect(r"info_extractor/streets_NizhNov.db")
 
 def hi_command(bot, update):
     user = update.message.from_user
@@ -18,9 +21,22 @@ def hi_command(bot, update):
     msg = name + ', I\'m here to serve'
     bot.sendMessage(chat_id=update.message.chat_id, text=msg)
 
+def grep_command(bot, update):
+    msg_text = update.message.text.strip()
+    ndx = msg_text.find(' ')
+    if ndx == -1:
+        bot.sendMessage(chat_id=update.message.chat_id, text="grep WHAT?")
+    else:
+        c = conn.cursor()
+        res=""
+        for row in c.execute("SELECT name FROM streets WHERE name LIKE '%" + msg_text[ndx:] + "%'"):
+            res=res+row
+        bot.sendMessage(chat_id=update.message.chat_id, text=res)
+
 def help_command(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, 
                     text='<code>/hi</code> - bot will say something',
+                    text='<code>/grep</code> - search for smth in street name; case insensitive; ^ and $ are not supported',
                     parse_mode=ParseMode.HTML)
 
 def error(bot, update, error):
@@ -39,6 +55,7 @@ if __name__ == '__main__':
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("hi", hi_command))
+    dispatcher.add_handler(CommandHandler("grep", grep_command))
 
     dispatcher.add_error_handler(error)
 
