ba8b7a91bbeaadceccea201c853741065a88c68a
y.sergukova <y.sergukova@corp.mail.ru>
2017-08-24 17:55:37 +0300

    calculate zone

--- geo.py
+++ geo.py
@@ -3,6 +3,12 @@
 
 import math
 
+class Cortesian:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
 class GeoPoint:
     def __init__(self, lon, lat):
         self.lon = lon
@@ -11,8 +17,25 @@ class GeoPoint:
     def printl(self):
         print("P: lon=" + repr(self.lon) + ", lat=" + repr(self.lat))
 
-    def radian(self):
+    def degree2radian(self):
         return GeoPoint(self.lon*math.pi/180., self.lat*math.pi/180.)
+ 
+    def radian2degree(self):
+        return GeoPoint(self.lon*180./math.pi, self.lat*180./math.pi)
+
+    def cortesian(self):
+        rad = self.degree2radian()
+        X = math.cos(rad.lat) * math.cos(rad.lon)
+        Y = math.cos(rad.lat) * math.sin(rad.lon)
+        Z = math.sin(rad.lat)
+        return Cortesian(X, Y, Z)
+
+class GeoZone:
+    def __init__(self, point1, point2, offset):
+        wrapper = GeoWrapper()
+        self.central = wrapper.central(point1, point2)
+        self.radius = wrapper.distance(point1, self.central) + offset
+        self.bounds = wrapper.bounds(self.central, self.radius)
 
 class GeoWrapper:
     def __init__(self):
@@ -20,8 +43,8 @@ class GeoWrapper:
 
     def distance(self, point1, point2):
         radius = 6372795
-        rP1 = point1.radian()
-        rP2 = point2.radian()
+        rP1 = point1.degree2radian()
+        rP2 = point2.degree2radian()
 
         cl1 = math.cos(rP1.lat)
         cl2 = math.cos(rP2.lat)
@@ -36,4 +59,29 @@ class GeoWrapper:
         ad = math.atan2(y,x)
         dist = ad*radius
         return dist
+
+    def central(self, point1, point2):
+        cort1 = point1.cortesian()
+        cort2 = point2.cortesian()
+
+        x = cort1.x + cort2.x
+        y = cort1.y + cort2.y
+        z = cort1.z + cort2.z
+        Lon = math.atan2(y, x)
+        Hyp = math.sqrt(x * x + y * y)
+        Lat = math.atan2(z, Hyp)
+
+        return GeoPoint(Lon, Lat).radian2degree()
+
+    def bounds(self, central, distance):
+        kmInLongitudeDegree = 111.320 * math.cos( central.lat / 180. * math.pi)
+        deltaLat = distance / (1000.*111.1)
+        deltaLong = distance / (1000.*kmInLongitudeDegree)
+
+        bounds={}
+        bounds['minLat'] = central.lat - deltaLat
+        bounds['maxLat'] = central.lat + deltaLat
+        bounds['minLon'] = central.lon - deltaLong
+        bounds['maxLon'] = central.lon + deltaLong
+        return bounds
 
--- tests/test_geo.py
+++ tests/test_geo.py
@@ -7,9 +7,13 @@ from geo import GeoPoint, GeoWrapper
 class TestMain(unittest.TestCase):
     def test_basic(self):
         point = GeoPoint(1.1234, 45.983)
-        radPoint = point.radian()
+        radPoint = point.degree2radian()
         self.assertAlmostEqual(radPoint.lon, 0.0196070288, places=4, msg=None, delta=None)
         self.assertAlmostEqual(radPoint.lat, 0.80255475, places=4, msg=None, delta=None)
+
+        degPoint = radPoint.radian2degree()
+        self.assertAlmostEqual(degPoint.lon, point.lon, places=4, msg=None, delta=None)
+        self.assertAlmostEqual(degPoint.lat, point.lat, places=4, msg=None, delta=None)
 
     def test_distance(self):
         wrapper = GeoWrapper()
@@ -17,6 +21,19 @@ class TestMain(unittest.TestCase):
         self.assertAlmostEqual(wrapper.distance(GeoPoint(-139.398,77.1539), GeoPoint(-139.55,-77.1804))/100., 171660.29, places=2, msg=None, delta=None)
         self.assertAlmostEqual(wrapper.distance(GeoPoint(120.398,77.1539), GeoPoint(129.55,77.1804))/100., 2258.83, places=2, msg=None, delta=None)
         self.assertAlmostEqual(wrapper.distance(GeoPoint(-120.398,77.1539), GeoPoint(129.55,77.1804))/100., 23326.69, places=2, msg=None, delta=None)
+
+    def test_central(self):
+        wrapper = GeoWrapper()
+        p1 = GeoPoint(43.978471,56.319519)
+        p2 = GeoPoint(43.977447,56.319724)
+        c = wrapper.central(p1, p2)
+        self.assertAlmostEqual(wrapper.distance(p1, c)/100., wrapper.distance(p2, c)/100., places=2, msg=None, delta=None)
+
+        bounds=wrapper.bounds(c, 30)
+        print(wrapper.distance(c, GeoPoint(c.lon, bounds['minLat'])))
+        print(wrapper.distance(c, GeoPoint(c.lon, bounds['maxLat'])))
+        print(wrapper.distance(c, GeoPoint(bounds['minLon'], c.lat)))
+        print(wrapper.distance(c, GeoPoint(bounds['maxLon'], c.lat)))
 
 if __name__ == '__main__':
     unittest.main()
