fa5b27e4bcdabe8692b0537c47a92558f6c1f85c
y.sergukova <y.sergukova@corp.mail.ru>
2017-08-24 14:14:01 +0300

    fix tests

--- tests/test_dbhelper.py
+++ tests/test_dbhelper.py
@@ -17,7 +17,7 @@ class TestMain(unittest.TestCase):
     def test_grep_list(self):
         db = DBHelper()
 
-        text = u""
+        text = u"улица Генерала Зимина\nулица Генерала Штеменко\nулица Им Генерала Клюева\nулица имени Генерала Ивлиева И.Д."
 
         self.assertEqual(text, db.get_names("Генерал"))
         self.assertEqual(text, db.get_names("генерал"))
